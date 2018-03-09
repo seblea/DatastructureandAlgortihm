54486a018b38c5da432be48cb75b4f5480b3b752
seblea <seble_ashenafi@yahoo.com>
2018-03-09 15:59:13 -0800

    update

---
+++ Strings/LongSubSequence.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    public static class LongSubSequence
+    { 
+        public static char[] Long(string st1, string st2)
+        {
+            int m = st1.Length + 1;
+            int n = st2.Length + 1;
+            int[,] mat = new int[m, n];
+            int max = 0;
+            int count = 0;
+            int i, j,s; // counter for the loop
+
+            char[] st;
+            
+
+            // let construct  m X n matrics
+
+            for(i =0; i < m; i++)
+            {
+                for( j=0; j< n; j++)
+                {
+                    if(i==0 || j == 0)
+                    {
+                        mat[i, j] = 0;
+                    }
+
+                    else
+                    {  if (st1[i - 1] == st2[j - 1])  // if eqaul go back diagonal and add 1
+                        {
+                            mat[i, j] = mat[i - 1, j - 1] + 1;
+                        }
+                        else // not eqaul take maximum of left or up cell 
+
+                            mat[i, j] = mat[i, j - 1] >= mat[i - 1, j] ? mat[i, j - 1] : mat[i - 1, j];
+
+                    }
+
+                }
+            }
+
+            max = mat[m - 1, n - 1]; // length of the subsequence
+
+            st = new char[max]; // to hold the subsequence character
+            // back track the sub sequence
+
+            i = m - 1;
+            j = n - 1;
+            s = max - 1;
+
+            while(count < max)
+            {
+                if (st1[i - 1] == st2[j - 1]) // if it is much pick the charachter and go back diagonal
+                {
+                    count++;
+                    st[s] = st1[i - 1];
+                    i = i - 1;
+                    j = j - 1;
+                }
+                else // select from the top and left whic is maximum , default to left
+                {
+                    if(mat[i - 1,j] > mat[i,j-1] )
+                      {
+                        i = i - 1;
+
+                      }
+                    else
+                     {
+                        j = j - 1;
+                     }
+                }
+
+
+
+            }
+
+           
+            return st;
+        }
+    }
+}
---
+++ Strings/ShortestString.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    public static class ShortestString
+    { //given two string st1 and st2,find the shortest string that has both st1 and st2 as subsequence
+
+        public static char[] ShortString(string st1, string st2)
+        {
+            // let first get the largest common subsequence
+
+            char[] st = LongSubSequence.Long(st1, st2);
+
+
+            int ln1, ln2, ln3, i;
+            ln1 = 0;
+            ln2 = 0;
+            ln3 = 0;
+
+
+            char[] sst = new char[st1.Length + st2.Length];
+            i = 0;
+
+            while (ln1 < st1.Length || ln2 < st2.Length || ln3 < st.Length)
+            {
+                if (ln1 < st1.Length && ln2 < st2.Length)
+                {
+                    if (st1[ln1] != st[ln3])
+                    {
+                        sst[i] = st[ln1];
+                        ln1++;
+                        i++;
+                    }
+
+                    else if (st1[ln2] != st[ln3])
+                    {
+                        sst[i] = st[ln2];
+                        ln2++;
+                        i++;
+                    }
+
+                    else
+                    {
+                        sst[i] = st[ln2];
+                        ln2++;
+                        ln3++;
+                        i++;
+                    }
+                }
+                else if (ln1 < st1.Length)
+                {
+                    sst[i] = st[ln1];
+                    ln1++;
+                    i++;
+                }
+                else
+                {
+
+                    sst[i] = st[ln2];
+                    ln2++;
+                    i++;
+                }
+
+            }
+
+            return sst;
+        }
+    }
+}
