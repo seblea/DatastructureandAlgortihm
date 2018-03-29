5fc4aceebbb2c314ec3643e5ca2d40d45d0063d3
seblea <seble_ashenafi@yahoo.com>
2018-03-28 19:57:03 -0700

    update

--- BinarySearchTree/AllPathSumtogivenvalue.cs
+++ BinarySearchTree/AllPathSumtogivenvalue.cs
@@ -12,6 +12,7 @@ namespace BinaryTree
        public static void AllPathSumto(BinaryTreeNode ro, int val)
         {
             List<Stack<int>> ans=new List<Stack<int>>(); 
+           
             List<Stack<int>> allpath;
             allpath = ValSum(ro, val, ref ans);
 
@@ -35,10 +36,11 @@ namespace BinaryTree
             List<Stack<int>> left = null;
             List<Stack<int>> right = null;
             List<Stack<int>> allnode = new List<Stack<int>>();  // to return to parent possible path from all leaf node to them 
-            
-            
+            Stack<int> res = new Stack<int>();
+
+
             int sum = 0;
-         
+
             int ro = (int)root.data; // data is object type
 
             if (root == null)
@@ -49,46 +51,56 @@ namespace BinaryTree
 
             if (root.right != null)
                 right = ValSum(root.left, val,ref ans);
-            
-            // for each path push the current value , to pass for the parent
-            foreach (Stack<int> st in left)
+
+
+
+            // for each path , from leaf nodes to current node, push the current value  
+
+            foreach (Stack<int> st in left) // left childerns 
             {
 
                 st.Push(ro);
             }
 
-            foreach (Stack<int> rst in right)
+            foreach (Stack<int> rst in right) //right childrens
             {
                 rst.Push(ro);
             }
 
-            // chech all path the sum 
-            foreach (Stack<int> stal in left)
+            // chech all path for the sum 
+            foreach (Stack<int> stal in left) // left childrens and the current node 
             {
                 sum = 0;
                 foreach (int t in stal)
                 {
                     sum = sum + t;
+                    res.Push(t);
+                    if (sum == val)
+                        break;
                 }
 
-                // if so add to ref list the stack
+                // if so add the stack to the list. Possible path for the sum 
                 if (sum == val)
-                    ans.Add(stal);
+                    ans.Add(res);
             }
 
+            res.Clear();
             // chech all path the sum 
-            foreach (Stack<int> star in right)
+            foreach (Stack<int> star in right) // childrens and the current node
             {
                 sum = 0;
                 foreach (int t in star)
                 {
                     sum = sum + t;
+                    res.Push(t);
+                    if (sum == val)
+                        break;
                 }
 
-                if (sum == val)
-                    ans.Add(star);
+                if (sum == val) // 
+                    ans.Add(res);
             }
-            //merege left and right ,merege into right , all possible path from leaf to the parent 
+            //merege left and right , default merege into right , all possible path from leaf to the parent 
             foreach (Stack<int> stal in left)
                 right.Add(stal);
 
